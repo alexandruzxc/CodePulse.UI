e50dfa66bc8ea9def3c207a06e5509755958c572
Alexandru Ifrim <ifrim.alexandru0@gmail.com>
2024-05-20 12:47:13 +0300

    initial commit

---
+++ src/app/features/public/blog-details/blog-details.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Observable } from 'rxjs';
+import { BlogPost } from '../../blog-post/models/blog-post.model';
+import { BlogPostService } from '../../blog-post/services/blog-post.service';
+
+@Component({
+  selector: 'app-blog-details',
+  templateUrl: './blog-details.component.html',
+  styleUrls: ['./blog-details.component.css']
+})
+export class BlogDetailsComponent implements OnInit {
+  url: string | null = null;
+  blogPost$? : Observable<BlogPost>;
+
+  constructor(private route: ActivatedRoute,
+    private blogPostSerice: BlogPostService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap
+    .subscribe({
+      next: (params) => {
+        this. url = params.get('url');
+      }
+    });
+
+    // Fetch blog details by url
+    if (this.url) {
+      this.blogPost$ = this.blogPostSerice.getBlogPostByUrlHandle(this.url);
+    }
+
+  }
+
+
+
+
+
+}
